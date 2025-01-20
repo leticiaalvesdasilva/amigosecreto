4ac7f7d5c4b8aecfde4f538e0826f3bbce43a6df
Leticia <alvesleticia42@yahoo.com>
2025-01-20 10:13:20 -0300

    add array , if e else , innerHTML

--- app.js
+++ app.js
@@ -1,23 +1,41 @@
 let lista = [];
 
-
-
 function adicionarAmigo(){
     let isValid = validar();
     if (isValid == true){
         adicionar();
+        adicionarTela();
     }
 }
 
 function adicionar(){
     let input = document.getElementById('amigo');
-    lista.push (input.value);
+    let existeDuplicado = lista.find(nome => nome.toLowerCase() == input.value.toLowerCase());
+   
+    if (existeDuplicado !== undefined){
+        alert('nome duplicado: '+existeDuplicado);
+    }
+    else {
+        lista.push (input.value);
+    }
     console.log(lista);
+   
     input.value = '';
 }
 
+function adicionarTela(){
+    let ul = document.getElementById('listaAmigos');
+    ul.innerHTML = '';
+    
+    lista.forEach(nome => {
+        let li = document.createElement('li');
+        li.textContent = nome;
+        ul.appendChild(li);        
+    })
+}
+
 function validar(){
-    let input = document.getElementById('amigo');;
+    let input = document.getElementById('amigo');
 
     if ( input.value == ''){
         alert('Por favor, adicionar nome.');
@@ -26,3 +44,23 @@ function validar(){
 
     return true;
 }
+
+function sortearAmigo(){
+    let numero = parseInt(Math.random()* lista.length);
+    let nome = (lista[numero]);
+    lista = [];
+    adicionarTela();
+    resultado(nome);
+}
+
+function resultado(nome){
+    console.log('O nome sorteado é:' + nome);
+    let ul = document.getElementById('resultado');
+    ul.innerHTML = '';
+    let li = document.createElement('li');
+    li.textContent = 'O nome sorteado é:' + nome;
+    ul.appendChild(li);  
+
+}
+
+
